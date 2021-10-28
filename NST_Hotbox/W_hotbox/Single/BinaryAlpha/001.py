83e89b9fa6dd44aadbdbcbe81234cbd9ac453bdc
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 15:52:22 +0200

    Create 001.py

---
+++ NST_Hotbox/W_hotbox/Single/BinaryAlpha/001.py
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Analyze: RGBA
+# COLOR: #70919e
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+ns = nuke.selectedNodes()
+for n in ns:
+    n.knob('an').setValue('rgba')
