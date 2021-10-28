e345d32c225701c301b16cb6327ec62520ce1938
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 15:54:07 +0200

    Create 001.py

---
+++ NST_Hotbox/W_hotbox/Single/BlacksMatch/001.py
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Output: Alpha
+# COLOR: #699e69
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+ns = nuke.selectedNodes()
+for n in ns:
+    n.knob('output1').setValue('alpha')
